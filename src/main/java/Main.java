42a41b2e4c1ce0bd01596906238e572db4c14912
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-01-20 14:25:01 -0800

    Add Exclamation marks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello!!");
         System.out.println("Another print line");
         System.out.println("Adding a third print line");
     }
