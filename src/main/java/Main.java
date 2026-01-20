31611917117225fd5b86987fa60ada11b1a5874a
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-01-20 14:04:20 -0800

    add another print line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Another print line");
     }
 }
 
-// Version control - a tool for tracking changes
-// What is a repo? HelloWorldGithub is a folder or a "directory"
-// To make this folder a repository (or repo for short), I need to put it under version control.
+/*
+ Version control - a tool for tracking changes
+ What is a repo? HelloWorldGithub is a folder or a "directory"
+ To make this folder a repository (or repo for short), I need to put it under version control.
+ A commit is a package of changes with a label
+ label <---> message
+ */
+
