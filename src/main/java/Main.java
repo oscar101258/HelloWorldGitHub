a173241e475b10085494fa739d8804df3735dc05
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-01-20 13:58:58 -0800

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Oscar
+ * 08/27/25
+ * 0.1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+// Version control - a tool for tracking changes
+// What is a repo? HelloWorldGithub is a folder or a "directory"
+// To make this folder a repository (or repo for short), I need to put it under version control.
